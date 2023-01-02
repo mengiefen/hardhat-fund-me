773fb973d995bd289c74f1abb9f8578c7476a45a
mengiefen <mengiefen@gmail.com>
2023-01-02 12:18:47 +0300

    add deployment scripts

---
+++ scripts/withdraw.js
@@ -0,0 +1,20 @@
+const { getNamedAccounts, deployments, ethers } = require('hardhat');
+async function main() {
+    const { deployer } = await getNamedAccounts();
+    const fundMe = await ethers.getContract('FundMe', deployer);
+
+    console.log('Withdrawing ETH...');
+
+    const txResponse = await fundMe.withdraw();
+
+    await txResponse.wait(1);
+
+    console.log('Withdrawn!');
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
